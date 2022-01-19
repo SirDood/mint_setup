64b495f288be64d2fb12035cd7b05138751ad7e2
Izzat Z <izzat.zainir11@gmail.com>
2022-01-19 18:00:27 +0800

    Added print lines to init.sh

--- init.sh
+++ init.sh
@@ -7,14 +7,20 @@
 sudo apt update && sudo apt upgrade -y
 
 # Install git and xclip
+printf "\nInstalling git...\n"
 apt install git -y
+printf "\nInstalling xclip...\n"
 apt install xclip -y
 
 # Download repo
+printf "\nCreating temp directory..."
 mkdir ~/Downloads/temp
 cd ~/Downloads/temp
+printf "Cloning mint_setup into temp...\n"
 git clone https://github.com/SirDood/mint_setup.git
 
 # Run mint_setup.sh
+printf "\nRunning mint_setup.sh...\n"
 chmod +x ~/Downloads/temp/mint_setup/mint_setup.sh
+printf "\n\n"
 ~/Downloads/temp/mint_setup/mint_setup.sh
